3a36ac78280358196d4e60335c174d19ecc819e0
Bao <bao.tran@bluecomgroup.com>
2016-04-08 15:51:31 +0700

    update port for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js');
 
@@ -13,6 +14,6 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
 	console.log('server starting...');
 });
